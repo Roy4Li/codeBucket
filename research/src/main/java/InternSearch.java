8adbecfdcb310c346d9618561e6e6ffb4cd4897c
lileilei <lileilei@meituan.com>
2016-09-07 13:35:04 +0800

    string.intern()

---
+++ research/src/main/java/InternSearch.java
@@ -0,0 +1,20 @@
+/**
+ * Created by lilei on 16/9/7.
+ */
+public class InternSearch {
+
+    public static void main(String[] args) {
+        // JDK 1.7开始,intern()方法不再将字符串复制到永久代,而是在常量池中记录首次出现的示例引用
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern() == str1);
+
+        // 这个例子更好的说明了问题,不是首次出现的字符串常量
+        // todo 常量池是如何记录首次出现的字符串的引用的?
+        String str1_1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1_1.intern() == str1_1);
+
+        // "java"字符串不是首次出现的示例引用
+        String str2 = new StringBuilder("jav").append("a").toString();
+        System.out.println(str2.intern() == str2);
+    }
+}
--- simple/src/main/java/org/roy/lessons/joda/TimeDemo.java
+++ simple/src/main/java/org/roy/lessons/joda/TimeDemo.java
@@ -12,16 +12,28 @@ public class TimeDemo {
         LocalDate localDate = new LocalDate();
         System.out.println(localDate);
         System.out.println("day of week is : " + localDate.getDayOfWeek());
-        System.out.println("formatDatekey : "+ formatDatekey());
+
+        DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyyMMdd");
+        int datekey = 20160720;
+        LocalDate localDate1 = dtf.parseLocalDate(Integer.toString(datekey));
+        int datekey2 = Integer.parseInt(dtf.print(localDate1));
+        System.out.println("datekey="+datekey +",datekey2="+datekey2);
+
+        System.out.println("上月1号: "+ dtf.print(localDate1.minusMonths(1)
+                .dayOfMonth()
+                .withMinimumValue()));
+
+        System.out.println("localDate1 changed to "+localDate1);
     }
 
     /**
      * datekey like 20160629
      */
-    private static LocalDate formatDatekey(){
-        int datekey = 20160629;
+    private static LocalDate formatDatekey(int datekey){
         DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyyMMdd");
         return dtf.parseLocalDate(Integer.toString(datekey));
     }
+
+
 
 }
