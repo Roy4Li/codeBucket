8bb8b958eda57ca9445ece6a96f53ec3f2f861f2
Roy4Li <enjoy_lilei@126.com>
2019-05-26 21:39:42 +0800

    Delete Node in a Linked List

---
+++ simple/src/main/java/org/roy/lessons/leetcode/Solution237.java
@@ -0,0 +1,27 @@
+package org.roy.lessons.leetcode;
+
+import com.google.common.collect.Lists;
+
+/**
+ * {@literal https://leetcode.com/problems/delete-node-in-a-linked-list/}
+ * BST,Binary Search Tree
+ */
+
+public class Solution237 {
+
+
+    public static void main(String[] args) {
+        Solution237 instance = new Solution237();
+        ListNode listNode = ListNode.buildFromList(Lists.newArrayList(4, 5, 1, 9));
+        instance.deleteNode(listNode);
+    }
+
+    public void deleteNode(ListNode node) {
+        // copy next to current,next never be null
+        ListNode next = node.next;
+        node.val = next.val;
+        // set pointer to the next.next
+        node.next = next.next;
+    }
+
+}
