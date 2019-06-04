a1790c8bff5d3b8c6df3681909e03b213efd231b
Roy4Li <enjoy_lilei@126.com>
2019-06-04 13:56:01 +0800

    fuzzbuzz

---
+++ simple/src/main/java/org/roy/lessons/xjtdd/fizzbuzz/FirstVersion.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2010-2018 meituan.com
+ * All rights reserved.
+ * 
+ */
+package org.roy.lessons.xjtdd.fizzbuzz;
+
+/**
+ * @author lilei
+ * @date 2019/06/04
+ */
+public class FirstVersion {
+
+    public void getVersion(){
+
+    }
+}
---
+++ simple/src/test/java/org/roy/lessons/xjtdd/fizzbuzz/FirstVersionTest.java
@@ -0,0 +1,13 @@
+package org.roy.lessons.xjtdd.fizzbuzz;
+
+
+/**
+ * Created by lilei on 2019/6/4.
+ */
+public class FirstVersionTest {
+
+    public void getVersion() throws Exception {
+
+    }
+
+}
