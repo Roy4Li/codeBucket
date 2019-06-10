32cd2f2c7b687394097fbe42dbb9c981e0fbd3f0
Roy4Li <enjoy_lilei@126.com>
2019-06-10 08:47:06 +0800

    add new class

---
+++ simple/src/main/java/org/roy/lessons/xjtdd/FizzBuzz.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2010-2018 meituan.com
+ * All rights reserved.
+ * 
+ */
+package org.roy.lessons.xjtdd;
+
+/**
+ * @author lilei
+ * @date 2019/06/10
+ */
+public class FizzBuzz {
+}
