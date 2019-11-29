e3f776d900f76c085cf8dd2389d50fafe7818d5d
roylee <enjoy_lilei@126.com>
2019-11-29 08:53:48 +0800

    passFirstTest

--- dojo/src/main/java/roylee/dojo/Args.java
+++ dojo/src/main/java/roylee/dojo/Args.java
@@ -1,38 +1,51 @@
 package roylee.dojo;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.isNull;
 
 public class Args {
-    private Map<String,Object> schemaMap;
+    private List<String> booleanSchemaList;
     private String[] arguments;
-
 
 
     public Args() {
     }
 
     public Args(String schema, String[] arguments) {
-        Args instance = new Args();
-        instance.setArguments(arguments);
-    }
-
-    public Map<String, Object> getSchemaMap() {
-        return schemaMap;
-    }
-
-    public void setSchemaMap(Map<String, Object> schemaMap) {
-        this.schemaMap = schemaMap;
-    }
-
-    public String[] getArguments() {
-        return arguments;
-    }
-
-    public void setArguments(String[] arguments) {
         this.arguments = arguments;
+        parseSchema(schema);
     }
 
-    public boolean getBoolean(String l) {
-        return true;
+    private void parseSchema(String schemaStr) {
+        String[] schemaArray = schemaStr.split(" ");
+        for (String schema : schemaArray) {
+            if (schema.length() == 1) {
+                addBooleanSchema(schema);
+            }
+        }
+    }
+
+    private void addBooleanSchema(String boolSchema) {
+        if (isNull(booleanSchemaList)) {
+            booleanSchemaList = new ArrayList<>();
+        }
+        booleanSchemaList.add(boolSchema);
+    }
+
+    public boolean getBoolean(String letter) {
+        if (!booleanSchemaList.contains(letter)) {
+            return false;
+        }
+        if (isNull(arguments) || arguments.length == 0) {
+            return false;
+        }
+        for (String arg : arguments) {
+            if (arg.equals("-" + letter)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
