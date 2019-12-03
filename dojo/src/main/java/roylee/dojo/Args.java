d6e81c677db47e4ee446e04535e1191286de03f2
roylee <enjoy_lilei@126.com>
2019-12-03 08:59:18 +0800

    add intArgMap

--- dojo/src/main/java/roylee/dojo/Args.java
+++ dojo/src/main/java/roylee/dojo/Args.java
@@ -1,12 +1,17 @@
 package roylee.dojo;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 public class Args {
-    private List<String> booleanSchemaList;
+    private List<Character> booleanSchemaList;
+    private List<Character> intSchemaList;
+    private Map<Character, Integer> intArgMap;
     private String[] arguments;
 
 
@@ -16,18 +21,55 @@ public class Args {
     public Args(String schema, String[] arguments) {
         this.arguments = arguments;
         parseSchema(schema);
+        parseArguments(arguments);
     }
 
-    private void parseSchema(String schemaStr) {
-        String[] schemaArray = schemaStr.split(" ");
-        for (String schema : schemaArray) {
-            if (schema.length() == 1) {
-                addBooleanSchema(schema);
+    private void parseArguments(String[] arguments) {
+        if (nonNull(intSchemaList) && intSchemaList.size() > 0) {
+            parseIntArgs(arguments);
+        }
+    }
+
+    private void parseIntArgs(String[] arguments) {
+        for (Character intSchema : intSchemaList) {
+            // find int value
+            for (int i = 0; i < arguments.length; i++) {
+                if (arguments[i].matches("-" + intSchema) && i + 1 < arguments.length) {
+                    String intStr = arguments[i + 1];
+                    addToIntArgMap(intSchema, Integer.parseInt(intStr));
+                }
             }
         }
     }
 
-    private void addBooleanSchema(String boolSchema) {
+    private void addToIntArgMap(Character intSchema, Integer intValue) {
+        if (isNull(intArgMap)) {
+            intArgMap = new HashMap<>();
+        }
+        intArgMap.put(intSchema, intValue);
+    }
+
+    private void parseSchema(String schemaStr) {
+        String[] schemaArray = schemaStr.split(",");
+        for (String schema : schemaArray) {
+            if (schema.length() == 1) {
+                addBooleanSchema(schema.charAt(0));
+            }
+            if (schema.length() == 2 && schema.matches("[a-zA-Z]#")) {
+                Character intSchema = schema.charAt(0);
+                addIntSchema(intSchema);
+            }
+        }
+    }
+
+    private void addIntSchema(Character intSchema) {
+        if (isNull(intSchemaList)) {
+            intSchemaList = new ArrayList<>();
+        }
+        intSchemaList.add(intSchema);
+    }
+
+    private void addBooleanSchema(Character boolSchema) {
         if (isNull(booleanSchemaList)) {
             booleanSchemaList = new ArrayList<>();
         }
@@ -47,5 +89,10 @@ public class Args {
             }
         }
         return false;
+    }
+
+    public int getInt(Character schemaChar) {
+
+        return Integer.parseInt(arguments[1]);
     }
 }
--- dojo/src/test/java/roylee/dojo/ArgsTest.java
+++ dojo/src/test/java/roylee/dojo/ArgsTest.java
@@ -8,8 +8,27 @@ import static org.junit.Assert.assertThat;
 public class ArgsTest {
 
     @Test
-    public void whenBooleanFlagThenOutputBoolean() {
-        assertThat(new Args("l", new String[]{"-l"}).getBoolean("l"), is(true));
+    public void whenBooleanFlagAndNoArgThenFalse() {
         assertThat(new Args("l", new String[]{""}).getBoolean("l"), is(false));
+    }
+
+    @Test
+    public void whenBooleanFlagAndHasArgThenArg() {
+        assertThat(new Args("l", new String[]{"-l"}).getBoolean("l"), is(true));
+    }
+
+
+    @Test
+    public void whenIntFlagAndHasArgThenArg() {
+        assertThat(new Args("p#", new String[]{"-p","123"}).getInt('p'), is(123));
+    }
+
+    @Test
+    public void whenIntFlagAndNoArgThenError() {
+        try {
+            new Args("p#", new String[]{"-l"}).getInt('p');
+        } catch (Exception e) {
+            assertThat(e.getMessage(), is("error"));
+        }
     }
 }
